240a07cdc16ab9e20bb274ee8b895816903b4cdf
HAKD@1 <dubeyad2003@gmail.com>
2023-09-01 23:51:43 +0530

    Fixed the bugs and handled the errors

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import WeatherInfo from "./components/WeatherInfo";
 import NavBar from "./components/NavBar";
 import Spinner from "./components/Spinner";
 import Footer from "./components/Footer";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   const [search, setSearch] = useState("Varanasi");
   const [today, setToday] = useState(true);
@@ -22,13 +24,33 @@ function App() {
   };
   useEffect(() => {
     const fetchWeather = async () => {
-      const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${
-          import.meta.env.VITE_API_KEY
-        }&units=metric `
-      );
-      const data = await res.json();
-      setWeatherData(data);
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${
+            import.meta.env.VITE_API_KEY
+          }&units=metric `
+        );
+        if (res.status === 404) {
+          throw new Error("Something went wrong!");
+        }
+        if (!res.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const data = await res.json();
+        setWeatherData(data);
+      } catch (error) {
+        toast(error.message, {
+          position: "top-right",
+          autoClose: 2000,
+          icon: "🚫",
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
     };
     fetchWeather();
   }, [search]);
@@ -39,6 +61,7 @@ function App() {
         style={{ userSelect: "none" }}
         className="bg-[#F1F6F9] bg-opacity-50"
       >
+        <ToastContainer type="error" />
         {/* Navigation Bar */}
         <NavBar
           handleSearch={handleSearch}
--- src/components/WeekHighlight.jsx
+++ src/components/WeekHighlight.jsx
@@ -6,25 +6,44 @@ import DayFour from "./week/DayFour";
 import DayFive from "./week/DayFive";
 import DaySix from "./week/DaySix";
 import DaySeven from "./week/DaySeven";
-
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function WeekHighlight({ weatherData, isCelcius }) {
   const [newWeatherData, setNewWeatherData] = useState(null);
   useEffect(() => {
     const fetchWeather = async () => {
-      const lat = weatherData.coord.lat;
-      const lon = weatherData.coord.lon;
-      const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${
-          import.meta.env.VITE_API_KEY
-        }&units=metric `
-      );
-      const data = await res.json();
-      setNewWeatherData(data);
+      try {
+        const lat = weatherData.coord.lat;
+        const lon = weatherData.coord.lon;
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${
+            import.meta.env.VITE_API_KEY
+          }&units=metric `
+        );
+        if (!res.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const data = await res.json();
+        setNewWeatherData(data);
+      } catch (error) {
+        toast(error.message, {
+          position: "top-right",
+          autoClose: 2000,
+          icon: "🚫",
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
     };
     fetchWeather();
   }, [weatherData]);
   return (
     <div className="my-10 mx-2 w-full">
+      <ToastContainer type="error" />
       <h1 className="text-2xl font-semibold ">Week Highlight</h1>
       {newWeatherData && (
         <div className="mt-5 space-x-5 flex">
